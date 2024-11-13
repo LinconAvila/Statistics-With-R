d2ef3529d6859971c1fe262083fe9c6230ff35a4
Lincon Avila <lincon1000ls@gmail.com>
2024-11-13 15:02:21 -0300

    Add student-enrollment-table exercise

---
+++ StudentEnrollmentTable/student-enrollment-table.r
@@ -0,0 +1,12 @@
+'Exercise: City A in 2010 had 19,286 students enrolled in elementary school, 
+1,681 in high school, and 234 in college. Organize this information in a table 
+(Adapted from Crespo, 2002).'
+
+# Creates the table with the provided data
+data <- data.frame(
+  Education_Level = c("Elementary School", "High School", "College"),
+  Number_of_Students = c(19286, 1681, 234)
+)
+
+# Displays the table
+print(data)
